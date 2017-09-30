0bf195ec509ae93f8812f710a7f25dc03080d66d
Henning Klatt <Henning@HP-260-6G.local>
2017-09-30 23:37:22 +0200

    Update

--- Camera/send.py
+++ Camera/send.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import subprocess
+from Preproc import predict
 
 
 verpasst = ["Online"]
@@ -11,17 +12,32 @@ chat_id = "XXX"
 
 
 while True:
+	takePicture()
+	result = predict("weights.hd5", "picture.jpg")	
+	send("Du bist im Stockwerk " + result + ".")
+
 	if len(verpasst) > 0:
 		for verpassten in verpasst:
 			send(verpassten)
 
 
 def send(text):
-	if "verschwoerhaus" in subprocess.check_output("netsh wlan show interfaces"):	
-		print("W-Lan Verfügbar")
-		response = os.system("ping -c 1 " + hostname)
-		if response == 0:
-			print("WWW Verfügbar")
-			os.system("curl -i -X GET https://api.telegram.org/bot" + bot_key + "/sendMessage?chat_id=" + chat_id + "&text=" + str(text))
+	if online():
+		os.system("curl -i -X GET https://api.telegram.org/bot" + bot_key + "/sendMessage?chat_id=" + chat_id + "&text=" + str(text))
+	else:
+		verpasst.append(text)
+
+
+def online():
+	if "verschwoerhaus" in subprocess.check_output("netsh wlan show interfaces"):
+                print("W-Lan Verfügbar")
+                response = os.system("ping -c 1 " + hostname)
+                if response == 1:
+                        return True
 		else:
-			verpasst.append(text)
+			return False
+	else:
+		return False
+
+def takePicture():
+	os.system("raspistill -vf -hf -w 96 -h 96 -o picture.jpg")
