9c2e1bffe6ff27acb8bbe445b15617751c01c162
SsimmmonnN <simon@pelz-hd.de>
2017-10-01 10:24:59 +0200

    Add crop()

--- Camera/checkPicture.py
+++ Camera/checkPicture.py
@@ -12,9 +12,11 @@ import os
 
 path = os.path.dirname(os.path.realpath(__file__))
 
+
 def loadModel(model, weights_path):
     model.load_weights(weights_path)
     return model
+
 
 def checkPic(model, pic):
     os.chdir(path)
@@ -22,7 +24,26 @@ def checkPic(model, pic):
     result = model.predict(np.reshape(pic, (1, 40, 40, 1)), 1, 1)
     return np.argmax(result)
 
-if __name__=="__main__":
+
+def crop(path):
+    """
+    Takes latest .jpg out of the given path
+    returns arrays showing 1. arrows 2. level
+    """
+    h = 40
+    w = 40
+    # offset in y direction
+    hoffPfeil = 15
+    hoffStock = 10
+    os.chdir(path)
+    f = glob.glob("*.jpg")[-1]
+    im = ndimage.imread(f)
+    arrows = im[hoffPfeil:h + hoffPfeil, w + 16:]
+    level = im[hoffStock:h + hoffStock, 0:w]
+    return arrows, level
+
+
+if __name__ == "__main__":
     os.chdir(path)
     ml = loadModel(LevelModel(), "levels.hd5")
     ma = loadModel(ArrowModel(), "arrows.hd5")
@@ -32,4 +53,3 @@ if __name__=="__main__":
     for i in glob.glob("Pictures/Levels/*.jpg"):
         print(i)
         print(checkPic(ml, sm.imread(i, "L")))
-
